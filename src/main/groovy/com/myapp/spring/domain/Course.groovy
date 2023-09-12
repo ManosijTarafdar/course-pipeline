9bdfbb3ba30990da88272e11254922f04bbf74f5
Manosij Tarafdar <mckvie.manosij@gmail.com>
2023-09-12 10:56:14 +0530

    fix:Annotation for course domain fixed

--- src/main/groovy/com/myapp/spring/domain/Course.groovy
+++ src/main/groovy/com/myapp/spring/domain/Course.groovy
@@ -2,11 +2,9 @@ package com.myapp.spring.domain
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import groovy.transform.ToString
 
 
 @Document(collection="coursedatabase")
-@ToString
 class Course {
 	
 	@Id
